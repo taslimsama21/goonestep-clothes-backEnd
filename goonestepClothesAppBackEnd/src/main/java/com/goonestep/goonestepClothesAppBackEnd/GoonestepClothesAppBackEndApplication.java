93bc16f928f24f5a723c3af4f9f1097d67a51b51
Taslim Sama <30925574+taslimsama21@users.noreply.github.com>
2020-05-13 16:12:26 +0530

    first commit

---
+++ goonestepClothesAppBackEnd/src/main/java/com/goonestep/goonestepClothesAppBackEnd/GoonestepClothesAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.goonestep.goonestepClothesAppBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoonestepClothesAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoonestepClothesAppBackEndApplication.class, args);
+	}
+
+}
---
+++ goonestepClothesAppBackEnd/src/test/java/com/goonestep/goonestepClothesAppBackEnd/GoonestepClothesAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goonestep.goonestepClothesAppBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoonestepClothesAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
